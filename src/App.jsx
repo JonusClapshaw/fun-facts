8e9ce8d40028c6be512c56315e6fdee6a154fa59
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-09 10:57:42 -0700

    hald way with css

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import FunFactCard from "./FunFactCard";
 function App() {
   return (
     <div>
-      <FunFactCard fact="I have visited more than 3 countries." />
-      <FunFactCard fact="I started programming in high school." />
-      <FunFactCard fact="I can solve a Rubik's cube." />
-      <FunFactCard fact="My first computer was a hand-me-down from 2004." />
-      <FunFactCard fact="I have a trained gaurd dog from slavaqia" />
+      <div>
+        <FunFactCard fact="I have visited more than 3 countries." />
+        <FunFactCard fact="I can solve a Rubik's cube." />
+        <FunFactCard fact="I started programming in college" />
+      </div>
+
+      <div>
+        <FunFactCard fact="I started programming in high school." />
+        <FunFactCard fact="My first computer was a hand-me-down from 2004." />
+        <FunFactCard fact="I have a trained gaurd dog from slavaqia" />
+      </div>
     </div>
   );
 }
