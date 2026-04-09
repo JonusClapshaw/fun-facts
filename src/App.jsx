4cf1bc85f5080afadf114bc2fb82d39a18f14ea9
Jonus <joclapshaw@gmail.com>
2026-04-09 10:43:00 -0700

    Step 3 implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import FunFactCard from "./FunFactCard";
 function App() {
   return (
     <div>
-      <FunFactCard fact="I have visited more than 10 countries." />
+      <FunFactCard fact="I have visited more than 3 countries." />
+      <FunFactCard fact="I started programming in high school." />
+      <FunFactCard fact="I can solve a Rubik's cube." />
+      <FunFactCard fact="My first computer was a hand-me-down from 2004." />
+      <FunFactCard fact="I have a trained gaurd dog from slavaqia" />
     </div>
   );
 }
